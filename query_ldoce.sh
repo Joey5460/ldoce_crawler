9f0f68cc5eacc4b9845856ab6b9b91e6f12e3048
zhouyu <zhouyu5460@gmail.com>
2017-03-13 11:22:56 +0800

    ldoce-3000-w.txt

--- query_ldoce.sh
+++ query_ldoce.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 case $1 in 
     s)
-        egrep 'S1|S2|S3' core_vocabulary.lm
+        egrep 'S1|S2|S3' core_vocabulary.txt
         ;;
-    t)
+    w)
+        egrep 'W1|W2|W3' core_vocabulary.txt
+        ;;
+    trim)
         #awk  '{OFS= "\t"}{for(i=2;i<5;i++) $i=""; print  $0}' $2 
         awk  '{printf("%15-s %s %s %s %s %s %s\n",$1,$5,$6,$7,$8,$9,$10);}' $2 
         ;;
