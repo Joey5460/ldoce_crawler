bafa13df36b6ca6684d92048e4557f334d1108c7
zhouyu <zhouyu5460@gmail.com>
2017-03-14 09:41:49 +0800

    added ldoce-freq

--- query_ldoce.sh
+++ query_ldoce.sh
@@ -10,6 +10,9 @@ case $1 in
         #awk  '{OFS= "\t"}{for(i=2;i<5;i++) $i=""; print  $0}' $2 
         awk  '{printf("%15-s %s %s %s %s %s %s\n",$1,$5,$6,$7,$8,$9,$10);}' $2 
         ;;
+    core)
+        egrep -v 'W1|W2|W3|S1|S2|S3' core_vocabulary.txt
+        ;;
     *)
         ;;
 esac
