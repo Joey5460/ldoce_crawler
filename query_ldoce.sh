021bdd69fca1f7fe988344e40568de08424c5fd6
zhouyu <zhouyu5460@gmail.com>
2017-03-14 11:40:01 +0800

    added low mid query

--- query_ldoce.sh
+++ query_ldoce.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/bash
+if [ -z $2 ]
+then
+    core_words='ldoce-core-vocabulary.txt'
+else
+	core_words=$2
+fi
+
 case $1 in 
     s)
-        egrep 'S1|S2|S3' core_vocabulary.txt
-        ;;
+        if [ -z $2 ]
+        then
+            egrep 'S1|S2|S3' $core_words
+        else
+            egrep 'S1|S2|S3' $2
+        fi
+    ;;
     w)
-        egrep 'W1|W2|W3' core_vocabulary.txt
-        ;;
+        if [ -z $2 ]
+        then
+            egrep 'W1|W2|W3' $core_words
+        else
+            egrep 'W1|W2|W3' $2
+        fi
+    ;;
     trim)
         #awk  '{OFS= "\t"}{for(i=2;i<5;i++) $i=""; print  $0}' $2 
         awk  '{printf("%15-s %s %s %s %s %s %s\n",$1,$5,$6,$7,$8,$9,$10);}' $2 
         ;;
-    core)
-        egrep -v 'W1|W2|W3|S1|S2|S3' core_vocabulary.txt
+    freq)
+		egrep -v 'W1|W2|W3|S1|S2|S3' $core_words 
+        ;;
+    low)
+        egrep -v 'W1|W2|W3|S1|S2|S3' $core_words| egrep '●○○'   
+        ;;
+    mid)
+        egrep -v 'W1|W2|W3|S1|S2|S3' $core_words| egrep '●●○'   
+        ;;
+    high)
+        egrep -v 'W1|W2|W3|S1|S2|S3' $core_words| egrep '●●●' 
         ;;
     *)
         ;;
